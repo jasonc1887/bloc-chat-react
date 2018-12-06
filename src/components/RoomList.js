a97836ae250657b1ea26c61b82c0295d21b76796
Jason Chavez <jasonc1887@gmail.com>
2018-12-06 07:52:14 -0700

    checkpoint 3 bloc chat:slack help commit

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,19 +5,32 @@ class RoomList extends Component {
     super(props);
 
     this.state = {
-      rooms: []
+      rooms: [],
+
+      newRoomName: ''
     };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
-    };
 
+  };
 
-    componentDidMount() {
-      this.roomsRef.on('child_added', snapshot => {
-      const room = snapshot.val();
-      room.key = snapshot.key;
-      this.setState({ rooms: this.state.rooms.concat( room ) });
-  });
+componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+    const room = snapshot.val();
+    room.key = snapshot.key;
+    this.setState({ rooms: this.state.rooms.concat( room ) })
+ });
+};
+
+handleChange(e) {
+  this.setState({ newRoomName: e.target.value })
+};
+
+createRoom(e) {
+  e.preventDefault();
+  if (!this.state.newRoomName) {return}
+  const newRoom = { rooms: this.state.newRoomName };
+  this.setState({ rooms: [...this.state.rooms, newRoom], newRoomName: '' });
 };
 
 render() {
@@ -31,6 +44,14 @@ render() {
             </tr>
            )
           }
+          <tr>
+            <td>
+              <form onSubmit={ (e) => this.createRoom(e) }>
+                <input type="text" value={ this.state.newRoomName } onChange={ (e) => this.handleChange(e) } />
+                <input type="submit" />
+              </form>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
