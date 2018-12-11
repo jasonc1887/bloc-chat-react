1e36e3921cae7d1f1ced3728f32e9e57b4fa66cc
Jason Chavez <jasonc1887@gmail.com>
2018-12-11 15:50:29 -0700

    checkpoint 4 bloc chat: completed

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -26,22 +26,20 @@ class MessageList extends Component {
     return(
       <section className="message-list">
         <div className="messages-per-room">
-          {this.state.messages.filter((message) => {
-
-            console.log('MESSAGE ROOM ID:', message.roomId)
-            console.log('ACTIVE ROOM ID:', this.props.activeRoom)
-
-          if (message.roomId === this.props.activeRoom) {
-          }}).map((message) =>
-        <div>
-          <p id = "username">{message.username}</p>
-          <p id = "time">{message.sentAt}</p>
-          <p id = "content">{message.content}</p>
-        </div>)}
+          {
+            this.state.messages.filter((message) => this.props.activeRoom === message.roomId)
+              .map((message) =>
+                <div>
+                  <p id="username">{message.username}</p>
+                  <p id="time">{message.sentAt}</p>
+                  <p id="content">{message.content}</p>
+                </div>
+              )
+          }
         </div>
       </section>
     );
-  }
+  } 
 }
 
 export default MessageList;
